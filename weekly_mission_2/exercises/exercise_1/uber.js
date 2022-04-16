b47b5c98ba3f832d5960db37b0010726c2e4c16a
olarag <oscar@difusion.mx>
2022-04-16 18:43:49 -0500

    se agregó contenido

---
+++ weekly_mission_2/exercises/exercise_1/uber.js
@@ -0,0 +1,18 @@
+const datos = {
+ name: "Carl's JR",
+ author: "carls-js",
+ rating: 4.7,
+ ratings: 254,
+ numberOfProducts: 8,
+ type: "Hamburguesas",
+ getProduct: function(){
+   return `Has ${this.numberOfProducts} and ${this.rating} of rating`
+ },
+ getGeneralInfo: function(){
+   return `This Uber Eats is from ${this.name} was created by ${this.author}`
+ }
+}
+
+console.log("Nombre del usuario:" + datos.name)
+console.log(datos.getProduct())
+console.log(datos.getGeneralInfo())
