3ea07ecd4596b93ea2843fbd7b27fd2e1edfc83b
Oscar Lara <oscar@neuf.mx>
2022-04-16 13:39:37 -0500

    se agregó contenido

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,10 @@
+// exports a class
+export default class MyPokemon {
+  constructor (name) {
+    this.name = name
+  }
+
+  sayHello() {
+    console.log('Mi pokemon ' + this.name + ' te saluda  ' )
+  }
+}
